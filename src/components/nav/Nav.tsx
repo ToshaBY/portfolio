99d0b32287389018a51be18c962bab0068f7221f
ToshaBY <anton.rybakou@gmail.com>
2022-07-12 20:29:32 +0300

    feat: add react-scroll

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import style from './Nav.module.css';
+import { Link } from 'react-scroll';
 
 export const Nav = React.memo(() => {
     return (
         <div className={style.nav}>
-            <a href="</>">Main</a>
-            <a href="</>">Skills</a>
-            <a href="</>">Projects</a>
-            <a href="</>">Contacts</a>
+            <Link to='/' spy={true} smooth={true} offset={50} duration={500}>Main</Link>
+            {/*<a href="</>">Main</a>*/}
+            {/*<a href="</>">Skills</a>*/}
+            {/*<a href="</>">Projects</a>*/}
+            {/*<a href="</>">Contacts</a>*/}
         </div>
-    );
+    )
 })
