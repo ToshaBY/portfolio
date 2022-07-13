e6c6f678755d2b25c546254142cc9462977d4ecf
ToshaBY <anton.rybakou@gmail.com>
2022-07-14 00:19:51 +0300

    feat: add media query, add *.pdf module, add Resume to download,

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import style from './Footer.module.scss';
 import styleContainer from "../../common/styles/Container.module.scss";
 import {Title} from "../../common/components/title/Title";
-import {faGithubSquare, faLinkedin, faTelegram} from "@fortawesome/free-brands-svg-icons";
+import {faGithub, faLinkedin, faTelegram} from "@fortawesome/free-brands-svg-icons";
 import {SocialItem} from "./socialItem/SocialItem";
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 
 
 export const Footer = React.memo(() => {
     const socialLinks = [
-        {id: 1, icon: faGithubSquare, link: 'https://github.com/ToshaBY'},
+        {id: 1, icon: faGithub, link: 'https://github.com/ToshaBY'},
         {id: 2, icon: faLinkedin, link: 'https://www.linkedin.com/in/tosharybakov/'},
         {id: 3, icon: faTelegram, link: 'https://t.me/untosha'},
         {id: 4, icon: faEnvelope, link: 'mailto:anton.rybakou@gmail.com'},
--- src/components/header/nav/Nav.tsx
+++ src/components/header/nav/Nav.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Nav.module.css';
-import {Link} from 'react-scroll'
+import {Link} from 'react-scroll';
+import CV from '../../../assets/files/CV.pdf';
 
 export const Nav = React.memo(() => {
     return (
@@ -8,15 +9,22 @@ export const Nav = React.memo(() => {
             <Link to='main' spy={true} smooth={true} offset={0} duration={500}>
                 Main
             </Link>
+
             <Link to='skills' spy={true} smooth={true} offset={0} duration={500}>
                 Skills
             </Link>
+
             <Link to='projects' spy={true} smooth={true} offset={0} duration={500}>
                 Projects
             </Link>
+
             <Link to='contacts' spy={true} smooth={true} offset={0} duration={500}>
                 Contacts
             </Link>
+
+            <a href={CV} target='_blank' rel={'noreferrer'} download={'Anton Rybakou CV.pdf'}>
+                Resume
+            </a>
         </nav>
     )
 })
